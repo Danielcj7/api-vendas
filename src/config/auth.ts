58ef8e44ebcbf145d5d00efd84ef3df63f5f41b2
Danielcj7 <daniel.cordeirojacinto@gmail.com>
2022-10-07 12:07:25 -0300

    Atualização: Gerenciamento de senhas e usuario. Seção 6, cap 63

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'c9ceb9e1b85d764c213fe32fb5f1451b',
+    expiresIn: '1d',
+  },
+};
---
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import AppError from '@shared/errors/AppError';
+import authConfig from '@config/auth';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function isAuthenticated(
+  request: Request,
+  reponse: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing.');
+  }
+  //Bearer hr325ehuai5vogpepspdje432
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT Token.');
+  }
+}
